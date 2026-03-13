3ed7f80fb78c8238753ca8b329138098116af00c
Rizwan-kambooh <www.rizwankambooh6@gmail.com>
2026-03-13 12:15:56 +0500

    Closure Examples

--- closure/index.js
+++ closure/index.js
@@ -7,3 +7,27 @@ function outerFunction() {
 }
 const closureFunction = outerFunction();
 console.log(closureFunction()); // logs: 'I am from the outer function'
+
+// another example of this is:
+function counter(value1) {
+    return function(value2){
+        return value1 * value2;
+    }
+}
+const count1 = counter(5);
+console.log(count1(2)); // logs: 10
+console.log(count1(3)); // logs: 15
+
+
+// another real world example of closure is when we want to create a function that generates a unique ID for each user. We can use a closure to keep track of the last ID generated and increment it each time a new ID is requested.
+function createIDGenerator() {
+    let lastID = 0;
+    return function() {
+        lastID++;
+        return lastID;
+    }
+}
+const generateID = createIDGenerator();
+console.log(generateID()); // logs: 1
+console.log(generateID()); // logs: 2
+console.log(generateID()); // logs: 3
